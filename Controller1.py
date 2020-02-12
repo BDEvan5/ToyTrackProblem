6d2fb84b28b0f9bd71a9438e25a159ffd908c127
Benjy <19811799@sun.ac.za>
2020-02-12 18:47:48 +0200

    Started the class

---
+++ Controller1.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+class Controller:
+    # this function must give some controll to the car
+    # I think that this function will be housed inside the car
+    # the function must accept target and then control the car towards the target
+    def __init__(self):
+        
