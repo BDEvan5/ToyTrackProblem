bec288185b5af4a5c60d4a47e095560e7fb0de49
Benjamin <benjaminevans316@gmail.com>
2020-10-01 12:06:20 +0000

    Mod finished for now in testing

--- TestingICRA.py
+++ TestingICRA.py
@@ -1,8 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
-
-import torch
 
 from TrackSimulator import TrackSim
 from RaceTrackMap import TrackMap
@@ -148,94 +145,6 @@ def train_mod(agent_name):
     return rewards
 
 
-
-"""Training functions: PURE REP"""
-def collect_rep_ep_obs(vehicle, env, buffer):
-    # env.env_map.reset_map()
-    wpts = vehicle.init_plan(env.env_map)
-    done, state, score = False, env.reset(None), 0.0
-    while not done:
-        action = vehicle.train_act(state)
-        vehicle.add_mem_step(buffer, state)
-        s_p, r, done, _ = env.step(action, updates=20)
-
-        state = s_p
-        # env.render(True)
-    
-    vehicle.show_history()
-    env.render_snapshot(wpts=wpts)
-    if r == -1:
-        print(f"The vehicle has crashed: check this out")
-    print(f"Ep done in {env.steps} steps --> B Number: {buffer.length}")
-
-def RaceRepVehicle(agent_name):
-    print(f"Testing vehicle performance")
-    env_map = TrackMap('TrackMap1000')
-
-    env = TrackSim(env_map)
-    vehicle = RepRaceVehicle(agent_name, True)
-
-    # env_map.reset_map()
-    wpts = vehicle.init_plan(env_map)
-    done, state, score = False, env.reset(None), 0.0
-    while not done:
-        action = vehicle.act(state)
-        s_p, r, done, _ = env.step(action, updates=20)
-        state = s_p
-        # env.render(False, wpts)
-
-    vehicle.show_history()
-    env.render_snapshot(wpts=wpts)
-    if r == -1:
-        print(f"The vehicle has crashed: check this out")
-    plt.show()
-    print(f"Ep done in {env.steps} steps ")
-
-def TrainRepTrack(agent_name, load=False):
-    env_map = TrackMap('TrackMap1000')
-
-    buffer = ReplayBufferSuper()
-    env = TrackSim(env_map)
-    vehicle = RepTrainVehicle(agent_name, load)
-
-    print(f"Creating data")
-    i = 0
-    while buffer.length < 1000:
-        collect_rep_ep_obs(vehicle, env, buffer)
-        vehicle.agent.train(buffer, 100)
-
-        if i % 5 == 0:
-            collect_rep_ep_obs(vehicle, env, buffer)
-        i += 1
-
-    vehicle.agent.save()
-
-    print(f"Starting training")
-    score, losses = 0, []
-    for n in range(10000):
-        l = vehicle.agent.train(buffer)
-        score += l 
-
-        if n % 100 == 1:
-            losses.append(score)
-            vehicle.agent.save()
-            print(f"Loss: {score}")
-            score = 0
-
-            RaceRepVehicle(agent_name)
-
-
-
-
-"""Total functions"""
-
-def RunRepAgent():
-    agent_name = "TestingRep"
-
-    TrainRepTrack(agent_name, False)
-    # TrainRepVehicle(agent_name, True)
-
-    # RaceRepVehicle(agent_name)
 
 
 
